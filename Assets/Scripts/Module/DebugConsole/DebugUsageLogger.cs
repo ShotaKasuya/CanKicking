3626c72542f171678cdb155f28e10248b681745b
ShotaKasuya <22rd055@ms.dendai.ac.jp>
2025-06-30 23:05:26 +0900

    build warn

---
+++ Assets/Scripts/Module/DebugConsole/DebugUsageLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Module.DebugConsole
+{
+#if UNITY_EDITOR
+    public class DebugUsageLogger : IPreprocessBuildWithReport
+    {
+        public int callbackOrder => 0;
+
+        public void OnPreprocessBuild(BuildReport report)
+        {
+            var debugUsages = AssetDatabase.FindAssets("t:MonoScript")
+                .Select(AssetDatabase.AssetPathToGUID)
+                .Where(path => path.EndsWith(".cs"))
+                .Select(path => new { path, text = System.IO.File.ReadAllText(path) })
+                .Where(file => file.text.Contains("DebugConsole"))
+                .ToList();
+
+            if (debugUsages.Count > 0)
+            {
+                Debug.LogWarning("Debug Consoleの使用");
+                foreach (var usage in debugUsages)
+                {
+                    Debug.LogWarning($"- 使用ファイル: {usage.path}");
+                }
+            }
+        }
+    }
+#endif
+}
